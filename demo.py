23c1c2635f4cf6a8b9bdf1e668f497eeb0d58d27
pratiksha <pratikshavairagade02@gmail.com>
2025-10-11 19:29:24 +0530

    adding new

--- demo.py
+++ demo.py
@@ -1,8 +1,11 @@
+"""
+This is a module docstring for the file. (Addresses C0114)
+"""
 def hello():
-
-    """"""
-    this is demo
-
-    """"""
+    """
+    Prints a simple greeting. (Addresses C0112 and W0105)
+    """
+    # this is demo (This is now a comment, not a statement with no effect)
+    print("This is a demo of a corrected function.") # Added a statement to make the function useful
 
 hello()
